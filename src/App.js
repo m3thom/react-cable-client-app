9e0338697d5836ff53fe5a7edd7bbcc24275dab8
m3thom <39703347+m3thom@users.noreply.github.com>
2022-06-13 18:07:43 +0700

    Connect to the API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { createConsumer } from "@rails/actioncable"
+import './App.css'
 
 function App() {
+  const [messages, setMessages] = useState([])
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    let actionCableRef
+
+    const initSocket = () => {
+      actionCableRef = createConsumer('ws://localhost:3000/cable')
+      actionCableRef.subscriptions.create(
+        { channel: 'MessagesChannel' },
+        {
+          received: message => {
+            setMessages((messages) => [...messages, message])
+          }
+        }
+      )
+    }
+
+    initSocket()
+
+    return () => actionCableRef?.disconnect()
+  }, [])
+
+  useEffect(() => {
+    const fetchMessages = () => {
+      fetch('http://localhost:3000/messages')
+        .then(res => res.json())
+        .then(messages => setMessages(messages))
+    }
+    fetchMessages()
+  }, [])
+
+  const handleMessageSubmit = async () => {
+    const messageObj = {
+      message: {
+        content: input
+      }
+    }
+    const fetchObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(messageObj)
+    }
+    await fetch('http://localhost:3000/messages', fetchObj)
+
+    setInput('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Messages</h2>
+      <ul>
+        {
+          messages.map((message, i) =>
+            <li key={i}>{message.content}</li>)
+        }
+      </ul>
+
+      <input
+        value={input}
+        placeholder="Enter Message"
+        onChange={e => setInput(e.target.value)}
+      />
+
+      <button onClick={handleMessageSubmit}>
+        Submit
+      </button>
     </div>
   );
 }
